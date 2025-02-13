440e98de182fdd0ea3aa22903ec581109604feb8
harrybyrne100 <139022323+harrybyrne100@users.noreply.github.com>
2025-02-13 12:22:53 +0000

    Update geolocation.js

--- netlify/edge-functions/geolocation.js
+++ netlify/edge-functions/geolocation.js
@@ -1,12 +1,22 @@
 export default async (request, context) => {
-  // Get the geolocation data from the request
-  const geo = context.geo;
+  const geoData = {
+    geo: {
+      city: "Dublin",
+      country: { code: "IE", name: "Ireland" },
+      subdivision: { code: "L", name: "Leinster" },
+      timezone: "Europe/Dublin",
+      latitude: 53.3382,
+      longitude: -6.2591,
+      postalCode: "D02",
+    }
+  };
 
-  // Extract the country code
-  const countryCode = geo.country.code;
-
-  // Return the simplified JSON response
-  return new Response(JSON.stringify({ countryCode }), {
-    headers: { 'Content-Type': 'application/json' },
+  return new Response(JSON.stringify(geoData), {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",  // ✅ Fixes CORS issue
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    }
   });
 };
